d7ab39ad855c87ed4dce89a2eb6d10f0babc53b5
hanna <me@hanna.lol>
2026-01-31 17:21:56 +0000

    chore: add astronvim config

---
+++ neovim/.config/nvim/lua/plugins/user.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "edenEast/nightfox.nvim"
+  },
+  {
+    "astronvim/astroui",
+    opts = { colorscheme = "carbonfox" },
+  },
+  {
+    "sourcegraph/amp.nvim",
+    opts = { auto_start = true, log_level = "info" },
+    branch = "main",
+    lazy = false,
+  },
+}
