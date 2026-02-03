a8942f8d26d2f937049ec1dc23c8b78d3cde355a
hanna <me@hanna.lol>
2026-02-03 19:38:30 +0000

    refactor(neovim): use tabs instead of spaces

--- neovim/.config/nvim/lua/polish.lua
+++ neovim/.config/nvim/lua/polish.lua
@@ -2,6 +2,11 @@
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.smarttab = false
+vim.opt.expandtab = true
 vim.opt.relativenumber = false
 
 vim.api.nvim_create_autocmd("TermOpen", {
