28be72f6a9d1c2064290960fe1f04b67bc406c66
hanna <me@hanna.lol>
2026-02-03 19:40:39 +0000

    refactor(neovim): change tab size to 2 spaces

--- neovim/.config/nvim/lua/polish.lua
+++ neovim/.config/nvim/lua/polish.lua
@@ -2,10 +2,9 @@
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 4
-vim.opt.smarttab = false
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 vim.opt.relativenumber = false
 
